4666161dfd19cda78bf178ca632cfc84b74ec30e
Aluno <aluno@senac.com>
2016-06-24 10:19:24 -0300

    add rating bar

--- app/src/main/java/milton/myimc/ResultActivity.java
+++ app/src/main/java/milton/myimc/ResultActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
-    TextView tvResultado, tvImc;
+    TextView tvResultado, tvImc, tvValorAvaliacao;
     Button buttonReturn;
+    RatingBar ratingBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +23,10 @@ public class ResultActivity extends AppCompatActivity {
         tvResultado = (TextView) findViewById(R.id.textViewResultado);
         tvImc = (TextView) findViewById(R.id.textViewImc);
         buttonReturn = (Button) findViewById(R.id.buttonReturn);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        tvValorAvaliacao = (TextView) findViewById(R.id.tvValorAvaliacao);
 
+        addListenerOnRatingBar();
         showResult();
 
     }
@@ -43,6 +49,34 @@ public class ResultActivity extends AppCompatActivity {
         this.finish();
 
     }
+
+    public void addListenerOnRatingBar() {
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        tvValorAvaliacao = (TextView) findViewById(R.id.tvValorAvaliacao);
+
+        //se o valor de avaliação mudar,
+        //exiba o valor de avaliação atual no resultado (textview) automaticamente
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            public void onRatingChanged(RatingBar ratingBar, float avaliacao, boolean fromUser) {
+                tvValorAvaliacao.setText(String.valueOf(avaliacao));
+            }
+        });
+    }
+
+//    public void addListenerOnButton() {
+//        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+//        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+//
+//        //se o botão for clicado, exiba o valor de avaliação corrente.
+//        btnSubmit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(ResultActivity.this,
+//                        String.valueOf(ratingBar.getRating()),
+//                        Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
 
 
 }
